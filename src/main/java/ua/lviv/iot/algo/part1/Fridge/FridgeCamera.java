097b3e094cdf752829553d56bc7aa2588c2dd794
FoXm1 <119307136+Andriy-IR-15@users.noreply.github.com>
2023-04-08 17:17:09 +0300

    Add files via upload

---
+++ src/main/java/ua/lviv/iot/algo/part1/Fridge/FridgeCamera.java
@@ -0,0 +1,27 @@
+package ua.lviv.iot.algo.part1.Fridge;
+
+import lombok.ToString;
+
+@ToString
+public class FridgeCamera extends Fridge {
+    private final int numberOfEntries;
+    private final String tapeDriveType;
+    private final String sausageMovementType;
+    private final int tapeSpeed;
+    private final int maxSausageWeight;
+
+    public static final int VOLUME_PER_KILOGRAM = 10;
+
+    public FridgeCamera(int numberOfEntries, String tapeDriveType, String sausageMovementType, int tapeSpeed, int maxSausageWeight) {
+        this.numberOfEntries = numberOfEntries;
+        this.tapeDriveType = tapeDriveType;
+        this.sausageMovementType = sausageMovementType;
+        this.tapeSpeed = tapeSpeed;
+        this.maxSausageWeight = maxSausageWeight;
+    }
+
+    @Override
+    int getMaxUsableCapacity() {
+        return maxSausageWeight * VOLUME_PER_KILOGRAM;
+    }
+}
